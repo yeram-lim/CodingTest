bb43d808497cd9a35540159b9fb31f20887c4c14
yeram-lim <yeramyeye@gmail.com>
2022-03-19 04:35:33 +0900

    [이코테] 그리디 3-4 1이 될 때까지

---
+++ This is CodingTest(study)/part2/ch03 Greedy/3-41이될때까지.py
@@ -0,0 +1,13 @@
+def toOne():
+    N, K = map(int, input().split())
+    answer = 0
+    while(N - 1):
+        if N % K == 0:
+            N = N / K
+            answer += 1
+        else:
+            N -= 1
+            answer += 1
+    print(answer)
+
+toOne()
